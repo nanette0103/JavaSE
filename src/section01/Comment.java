4d40986f3146efab10a59de50d81694493587ec8
sec <sec@DESKTOP-0G0FGBV>
2023-07-14 20:00:51 +0900

    First commit

---
+++ src/section01/Comment.java
@@ -0,0 +1,27 @@
+package section01
+/*
+ * 주석
+ * 코드에 영향을 주지 않는 개발자가 사용하는 메모 공간
+ * 줄삭제 : ctrl + d
+ * 
+ *  Alt + shift + j : Javadoc 주석
+ */
+
+ 
+/**
+ * Javadoc 주석
+ * 
+ * 입력받은 정수 출력하는 메소드
+ * 
+ * @param args
+ */
+public class Comment {
+	public static void main(String[] args) {
+//		한줄 주석
+		System.out.println("Hello, Java!");
+	}
+
+public static void printNum(int num) {
+		System.out.printlm("num: "+num)
+	}
+}
